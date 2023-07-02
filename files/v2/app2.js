01b0d9dfbfc76fc56fbe49a21e51fd7daf8e8ff2
Abdurauf Egamov <abuegamov03@gmail.com>
2023-07-02 17:59:07 +0500

    9

---
+++ files/v2/app2.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var filePath = '../files/v2/style.css';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'text';
+    xhr.open('GET', filePath, true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var fileContent = xhr.responseText;
+        var fileContentElement = document.getElementById('file-content2');
+        fileContentElement.textContent = fileContent;
+        var copyBtn = document.querySelector('.copy-btn2');
+        copyBtn.addEventListener('click', function() {
+          var range = document.createRange();
+          range.selectNodeContents(fileContentElement);
+          window.getSelection().removeAllRanges();
+          window.getSelection().addRange(range);
+          try {
+            var successful = document.execCommand("copy");
+            var message = successful ? 'Copied!' : 'Copy failed!';
+            console.log(message);
+            if (successful == true) {
+                copyBtn.innerHTML = "Nusxalandi!";
+            } else {
+                copyBtn.innerHTML = "Nusxalanmadi!";
+            }
+          } catch (err) {
+            console.log('Unable to copy');
+          }
+          window.getSelection().removeAllRanges();
+        });
+      }
+    };
+    xhr.send();
+});
+// @HanSiBinUz
